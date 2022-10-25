a1c0919797dddd1d5a3c60faca8f744ad599ba0f
notmaxwang <maxwang3737@gmail.com>
2022-10-24 23:09:28 -0700

    finished part 1

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,7 +1,26 @@
+const Flight = require('../models/flight');
+
 module.exports = {
-    new: newFlight
+    index,
+    new: newFlight,
+    create
+}
+
+function index(req, res) {
+    Flight.find({}, function(err, flights) {
+        res.render('flights/index', { flights });
+    });
 }
 
 function newFlight(req, res) {
     res.render('flights/new');
+}
+
+function create(req,res) {
+    const flight = new Flight(req.body);
+    flight.save(function(err) {
+        if (err) return res.redirect('flights/new');
+        console.log(flight);
+        res.redirect('flights');
+    });
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,6 +4,13 @@ const flightsCtrl = require('../controllers/flights');
 
 // All routes start with '/flights'
 
+router.get('/', flightsCtrl.index);
+
+
 router.get('/new', flightsCtrl.new);
+
+// POST /movies
+
+router.post('/flights', flightsCtrl.create)
 
 module.exports = router;
